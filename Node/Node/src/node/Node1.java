185f40510510061061b6378a247ddbc14b7ef0fd
tuhuynhkhtn <tuhuynhkhtn@gmail.com>
2018-06-15 17:37:30 +0700

    Node gửi danh sách file đến Server
    Node kết nối đến Server + gửi danh sách file đến Server

---
+++ Node/Node/src/node/Node1.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Node;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Tu Huynh
+ */
+public class Node1{
+    
+    //Liệt kê đường dẫn của tất cả các file trong thư mục
+    private void PrintDirectoryChildPath(File dir) {
+        File[] children = dir.listFiles();
+        for (File child : children) {
+            System.out.println(child.getAbsolutePath()); //getAbsolutePath: Lấy đường dẫn ra
+        }
+    }
+
+    //Liệt kê tên của tất cả các file trong thư mục 
+    private void PrintDirectoryChildName(File dir) {
+        String[] paths = dir.list();
+        for (String path : paths) {
+            System.out.println(path);
+        }
+    }
+    
+    private String[] BuiltDirectoryChildName(File dir) {
+        String[] paths = dir.list();
+        return  paths;
+    }
+    
+    public static void main(String argv[]) throws Exception {    
+        
+        String link = "D:\\GitHub\\JavaSocketDownloadFile\\Node\\Node\\Data\\Node1";
+        String port = "1111";
+        String modifiedSentence;
+
+        Socket clientSocket = new Socket("localhost", 6789);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());        
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
+
+        outToServer.writeUTF(port + '\n');
+        Node1 a = new Node1();
+        File dir = new File(link);
+        String[] Name = a.BuiltDirectoryChildName(dir);
+        for(String name : Name){
+            outToServer.writeUTF(name + '\n');
+        }        
+        
+        clientSocket.close();
+    }
+}
---
+++ Node/Node/src/node/Node3.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Node;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Tu Huynh
+ */
+public class Node3{
+    
+    //Liệt kê đường dẫn của tất cả các file trong thư mục
+    private void PrintDirectoryChildPath(File dir) {
+        File[] children = dir.listFiles();
+        for (File child : children) {
+            System.out.println(child.getAbsolutePath()); //getAbsolutePath: Lấy đường dẫn ra
+        }
+    }
+
+    //Liệt kê tên của tất cả các file trong thư mục 
+    private void PrintDirectoryChildName(File dir) {
+        String[] paths = dir.list();
+        for (String path : paths) {
+            System.out.println(path);
+        }
+    }
+    
+    private String[] BuiltDirectoryChildName(File dir) {
+        String[] paths = dir.list();
+        return  paths;
+    }
+    
+    public static void main(String argv[]) throws Exception {    
+        
+        String sentence = "D:\\GitHub\\JavaSocketDownloadFile\\Node\\Node\\Data\\Node3";
+        String port = "3333";
+        String modifiedSentence;
+
+        Socket clientSocket = new Socket("localhost", 6789);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        outToServer.writeBytes(port + '\n');
+        Node3 a = new Node3();
+        File dir = new File(sentence);
+        String[] Name = a.BuiltDirectoryChildName(dir);
+        for(String name : Name){
+            outToServer.writeBytes(name + '\n');
+        }
+        
+//        modifiedSentence = inFromServer.readLine();
+//        System.out.println("SERVER: " + modifiedSentence);
+//        modifiedSentence = inFromServer.readLine();
+//        System.out.println("        " + modifiedSentence);
+        
+        clientSocket.close();
+    }
+}
