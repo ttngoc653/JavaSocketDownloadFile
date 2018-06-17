32ddcf214bf463ed3789b18ed137826ce21246ce
Joss <Joss@C20180606.mshome.net>
2018-06-17 16:06:33 +0700

    server: create function soft and receive port of udp form note

--- Server/src/mainServer.java
+++ Server/src/mainServer.java
@@ -13,6 +13,26 @@ public class mainServer {
 	
 	private static ObjectInputStream ois=null;
 	private static ObjectOutputStream oos=null;
+	private static String file_name=null;
+	private static String port_udp_of_note=null;
+	
+	private static boolean softListFileName(){
+		try {
+			String temp="";
+			for (int i = 0; i < list_file.size()-1; i++)
+				for (int j = i+1; j < list_file.size(); j++) 
+					if(list_file.get(i).split("\t")[3].compareToIgnoreCase(list_file.get(j).split("\t")[3])<0) {
+						temp=list_file.get(i);
+						list_file.set(i, list_file.get(j));
+						list_file.set(j, temp);
+					}	
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		svr = new ServerSocket(1742);
@@ -31,7 +51,7 @@ public class mainServer {
 			//System.out.println(type);
 
 			oos=new ObjectOutputStream(sk.getOutputStream());
-			oos.writeObject("accepted");
+			//oos.writeObject("accepted");
 			// if sk is node
 			if(type.equals("0")) {
 				oos.writeObject("accepted");
@@ -39,30 +59,38 @@ public class mainServer {
 				sk.getInetAddress().toString();
 				sk.getPort();
 				
-				String file_name=ois.readObject().toString();
+				// nhận port udp từ note đã tạo gửi qua từ tcp
+				port_udp_of_note=ois.readObject().toString();				
+				oos.writeObject(port_udp_of_note);
+				
+				// nhận tên file đầu tiên
+				file_name=ois.readObject().toString();
 
 				while (!file_name.equals("end")) {
 					System.out.println("Received file name: "+file_name);
 					
-					list_file.add(sk.getInetAddress().toString()+"\t"+sk.getPort()+"\t"+file_name);
+					list_file.add(sk.getInetAddress().toString()+"\t"+sk.getPort()+"\t"+port_udp_of_note+"\t"+file_name);
 					//System.out.println(sk.getInetAddress().toString()+"_"+sk.getPort()+"_"+file_name);
 					
 					oos.writeObject("was");
 					file_name=ois.readObject().toString();
 				}
-				list_node.add(sk);
-			}else if(type.equals("1")) {				
-				System.out.print("Cliend connected!!!\t");
 				
-				for (Socket sk1 : list_node)
-					for (String str : args)
+				softListFileName();
+				
+				list_node.add(sk);
+			}else if(type.equals("1")) {
+				System.out.print("Cliend connected!!!\t");
+
+				for (String str : list_file)
+					for (Socket sk1 : list_node)
 						if(!sk1.isClosed() && str.split("\t")[1].equals(String.valueOf(sk1.getPort()))) {
-							oos.writeObject(str);
+							oos.writeObject(str.split("\t")[0]+"\t"+str.split("\t")[1]+"\t"+str.split("\t")[3]);
 									
 							ois.readObject();
 						}
 				
-				System.out.println("Sent all file name and IP + port (of node) to client");
+				System.out.println("Sent all file name and IP + port (UDP of node) to client");
 			}
 
 			oos.close();
